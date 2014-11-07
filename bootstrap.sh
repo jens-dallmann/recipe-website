b0d45c48ac58b24fff49ac65d36a535acc0d73ed
Jens Dallmann <jens.dallmann@posteo.de>
2014-11-07 16:02:48 +0100

    setup with example apache2

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,12 +3,12 @@ sudo apt-get install -y apache2 sshpass
 sudo a2enmod rewrite
 sudo a2enmod proxy
 sudo a2enmod proxy_http
-service apache2 start
+sudo service apache2 start
 
 sshpass -p 'vagrant' ssh -o StrictHostKeyChecking=no vagrant@deception uptime
-sshpass -p 'vagrant' scp -r deception:/home/deception/git/chef-tests/apache-conf/ .
+sshpass -p 'vagrant' scp -r vagrant@deception:/home/deception/git/chef-tests/apache-conf .
 
 sudo cp apache-conf/virtualbox.conf /etc/apache2/sites-available
 sudo ln -s /etc/apache2/sites-available/virtualbox.conf /etc/apache2/sites-enabled/virtualbox.conf
 
-service apache restart
+sudo service apache2 restart
