549833e56d7f7b73d8a3905385bf79b1486e42f7
Jens Dallmann <jens.dallmann@posteo.de>
2014-11-10 20:59:36 +0100

    fixed component scan

--- server/src/main/java/server/recipe/RecipeConfiguration.java
+++ server/src/main/java/server/recipe/RecipeConfiguration.java
@@ -1,9 +1,13 @@
 package server.recipe;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class RecipeConfiguration {
     @Bean
     public RecipeService recipeService() {
