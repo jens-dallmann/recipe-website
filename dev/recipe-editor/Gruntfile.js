2ab208961a69a896b86e81e30b502a4e5c13ea66
Hudson <hudson@mydomain.com>
2015-03-07 12:14:53 +0100

    cleanup

--- dev/recipe-editor/Gruntfile.js
+++ dev/recipe-editor/Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
-        distDir: 'src/main/webapp/',
+        distDir: 'target/resources',
         distDirCss: '<%= distDir %>/css',
         distDirJs: '<%= distDir %>/js',
         distDirImg: '<%= distDir %>/images',
